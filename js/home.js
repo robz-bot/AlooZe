049931ce6f645d4a8f25fb2794fc0035045ace8c
robinpromantus2022 <robinsiva1998@gmail.com>
2022-04-22 13:16:46 +0530

    Postal code added

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 const RANDOM_QUOTES_BASE_URL = new URL("https://api.quotable.io/random");
 var random_quote_text = document.querySelector("#random-quote");
 var random_quote_author = document.querySelector("#random-quote-author");
-var calc = document.querySelector("#calc");
-var calc1 = document.querySelector("#calc1");
+var card_img1 = document.querySelector("#card-img1");
+var card_img2 = document.querySelector("#card-img2");
 
 randomQuoteFunction();
 
@@ -23,20 +23,29 @@ async function randomQuoteFunction() {
         // console.log(data);
         random_quote_text.innerHTML = data.content;
         random_quote_author.innerHTML = data.author;
-        calc.setAttribute(
-                "src",
-                "https://source.unsplash.com/random/925X617/?currency"
-            ),
-            //working with title cards
-            renderCards();
+        render_card_images();
+        //working with title cards
+        renderCards();
     } catch (err) {
         console.log(err);
         // alert("Failed to fetch new quote");
     }
 }
 
+function render_card_images() {
+    card_img1.setAttribute(
+        "src",
+        "https://source.unsplash.com/random/925X617/?currency"
+    );
+    card_img2.setAttribute(
+        "src",
+        "https://source.unsplash.com/random/925X617/?postbox"
+    );
+}
+
 function renderCards() {
     card1Data();
+    card2Data();
 }
 
 function card1Data() {
@@ -44,4 +53,11 @@ function card1Data() {
         "<i class='bi bi-currency-exchange'></i> CER - Currency Exchange Rate";
     document.querySelector("#content1").textContent =
         "Currency exchange rate calculator helps you conversion of all major world currencies by latest foreign exchange rates.";
+}
+
+function card2Data() {
+    document.querySelector("#title2").innerHTML =
+        "<i class='bi bi-mailbox2'></i> PIN - Post Index Number";
+    document.querySelector("#content2").textContent =
+        "PIN allows to get details of Post Office by searching Postal PIN Code or Post Office Branch Name of India.";
 }
